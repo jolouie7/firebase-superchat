0e6f1e8b76249de06bacf1ecfd5f10fbb163d3db
JosephLouie <joseph161216@gmail.com>
2021-09-30 21:23:19 -0700

    Add firebase config

--- src/App.js
+++ src/App.js
@@ -8,7 +8,13 @@ import { useAuthstate } from 'react-firebase-hooks/auth';
 import { useCollection } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  // your config here
+  process.env.REACT_APP_apiKey,
+  process.env.REACT_APP_authDomain,
+  process.env.REACT_APP_projectId,
+  process.env.REACT_APP_storageBucket,
+  process.env.REACT_APP_messagingSenderId,
+  process.env.REACT_APP_appId,
+  process.env.REACT_APP_measurementId,
 });
 
 const auth = firebase.auth();
